ea5c293046b682df9dfff8118a912a94086bc3d2
Piyush <Piyush@192.168.2.2>
2018-02-17 10:11:21 +0530

    GIT Testing DEmo

---
+++ TestMaven/src/test/java/com/org/LearningMaven/TestMaven/TestGithub.java
@@ -0,0 +1,24 @@
+package com.org.LearningMaven.TestMaven;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TestGithub {
+	
+	
+	@Test
+	public void testJenkin(){
+		
+		
+		System.setProperty("webdriver.chrome.driver","C://Users//Piyush//Desktop//Selenium//new driver//chromedriver_win32//chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+
+		driver.manage().window().maximize();
+
+		driver.get("https://www.google.co.in/");
+		driver.close();
+		System.out.println("Welcome to Jenkins :");
+	}
+
+}
